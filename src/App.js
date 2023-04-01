173705130aeed27b1067d9a282d73ef46411e20d
Marcos Neto <marcosnto@MacBook-Air-de-Marcos.local>
2023-04-01 00:27:30 -0300

    create App.js and create Pet function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-const App = () => {
-    return React.createElement(
-        "div", 
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
-    )
-};    
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
-const container = document.getElementById('root');
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Bird",
+      animal: "Pepper",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Cat",
+      animal: "Doink",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
